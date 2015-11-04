de53091419bb5f0308486aff7a2d01f591ef00ad
Nikolay Nikolov <niksan9411@gmail.com>
2015-11-04 14:41:59 +0200

    rename main.cpp to f78397_3a.cpp

---
+++ AdvanceProgramming/Expresion_3a/f78397_3a.cpp
@@ -0,0 +1,79 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+/* Брой числа в редицата */
+  vector<int> vec;
+  bool is_done;
+
+void checkSol(int n,int sum)
+{
+    int temp_sum = 0;
+    for (int i = 0;i < n;i++)
+        temp_sum += vec[i];
+    if (temp_sum == sum)
+    {
+        int temp = 0;
+        for(int i = 0;i < n;i++)
+        {
+            if(vec[i] > 0)
+            {
+                cout << vec[i];
+                temp = i;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if(i != temp)
+            {
+                if (vec[i] > 0)
+                cout << "+" << vec[i];
+            else
+                cout << vec[i];
+            }
+        }
+        cout << "=" << temp_sum << endl;
+        is_done = true;
+    }
+}
+void variate(unsigned i,unsigned n,int sum)
+{
+    if(is_done)
+        return;
+    if (i >= n)
+    {
+        checkSol(n,sum);
+        return;
+    }
+    vec[i] = abs(vec[i]);
+    variate(i + 1,n,sum);
+    vec[i] = -abs(vec[i]);
+    variate(i + 1,n,sum);
+}
+void GetSumFromSequence(const vector<int> &vec,int sum)
+{
+    int n = vec.size()-1;
+    for(unsigned i = 0;i < vec.size() - 1;i++)
+        variate(i,n,sum);
+}
+
+int main(void) {
+    string input;
+    while(getline(cin,input))
+    {
+        istringstream stream_num (input);
+        int num;
+        while(stream_num >> num)
+            vec.push_back(num);
+        is_done = false;
+        int sum = vec[vec.size()-1];
+        vec[vec.size()-1] = 0;
+        GetSumFromSequence(vec,sum);
+        vec.clear();
+    }
+  return 0;
+}
