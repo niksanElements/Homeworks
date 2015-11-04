c73fd56be7c4fc4a37e806017c8b5cbd293e3e94
Nikolay Nikolov <niksan9411@gmail.com>
2015-11-04 19:07:18 +0200

    add Homework Expresion_3a

---
+++ AdvanceProgramming/Expresion_3b/f78397_3b.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+void CountDevNum(unsigned number_for_beaking,unsigned pos,const vector<int> &vec_bn);
+
+unsigned count;
+int adds[1000];
+
+int main()
+{
+    string input_breaking_numbers;
+    string input_numbers_for_breaking;
+    while(getline(cin,input_breaking_numbers) && getline(cin,input_numbers_for_breaking))
+    {
+        stringstream input_line_bn(input_breaking_numbers);
+        vector <int> breaking_numbers;
+        int temp = 0;
+        while(input_line_bn >> temp)
+            breaking_numbers.push_back(temp);
+        stringstream input_line_nfb(input_numbers_for_breaking);
+        vector<int> numbers_for_breaking;
+        while(input_line_nfb >> temp)
+            numbers_for_breaking.push_back(temp);
+        for (int i = 0;i < numbers_for_breaking.size();i++)
+        {
+            adds[0] = numbers_for_breaking[i]+1;
+            CountDevNum(numbers_for_breaking[i],1,breaking_numbers);
+            cout << count << " ";
+            count = 0;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+void CountDevNum(unsigned number_for_breaking,unsigned pos,const vector<int> &vec_bn)
+{
+    for (int i = vec_bn.size() - 1;i > 0;i--)
+    {
+        int curent_bn = vec_bn[i];
+        if(number_for_breaking > curent_bn)
+        {
+            adds[pos] = curent_bn;
+            if(adds[pos] <= adds[pos - 1])
+                CountDevNum(number_for_breaking - curent_bn,pos+1,vec_bn);
+        }
+        else if(number_for_breaking == curent_bn)
+        {
+            adds[pos] = curent_bn;
+            if(adds[pos] <= adds[pos - 1])
+                count++;
+        }
+    }
+}
