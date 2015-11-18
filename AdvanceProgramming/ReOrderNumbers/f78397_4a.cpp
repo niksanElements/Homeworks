76016b2381519521198c964e5dd00f7fe57ef7cd
Nikolay Nikolov <niksan9411@gmail.com>
2015-11-18 13:11:18 +0200

    add ex. 4a

---
+++ AdvanceProgramming/ReOrderNumbers/f78397_4a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool Compare (char i,char j) { return (i>j); }
+
+int main()
+{
+    string input;
+    while(cin >> input)
+    {
+        sort(&input[0],&input[input.size()-1],Compare);
+        cout << input << endl;
+    }
+    return 0;
+}
