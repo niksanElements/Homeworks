20d0640d61ea6f1b181eaefd99710117cc19f857
Nikolay Nikolov <niksan9411@gmail.com>
2015-11-22 22:29:34 +0200

    add ex. 5a

---
+++ AdvanceProgramming/Expresion_5a/f78397_5a.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double f(double x)
+{
+    return 2*x*x*x - 7*x - 97;   //f78397
+}
+
+bool biseccion(double a, double b, double tolerancia)
+ {
+        double c = 0;
+        do
+        {
+            c=(a+b)/2;
+            if(f(a)*f(c)<0)
+            {
+               b=c;
+            }
+            else
+            {
+               a=c;
+            }
+            if(f(a)*f(b) > 0)
+            {
+                cout << "NO SOLUTION";
+                return false;
+            }
+         }
+         while(abs(f(c)) > tolerancia);
+         cout << setprecision(11) << c  << ' '
+        << setprecision(11) << fixed << f(c) << endl;
+         return true;
+}
+
+int main()
+{
+    double tolerance = 10e-10;
+    biseccion(-10,10,tolerance);
+;
+}
